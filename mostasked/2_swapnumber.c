7d98b312b2f706df78dddd0aa34ba628d1ab6da0
MOHAMMAD SHADAB ABEDIN <shadababe04@gmail.com>
2024-08-30 19:20:46 +0530

    Swap two number without using third variable

---
+++ mostasked/2_swapnumber.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+// Swap two number without using third variable
+void swap(int *a, int *b)
+{
+    *a = *a + *b;
+    *b = *a - *b;
+    *a = *a - *b;
+}
+int main()
+{
+    int x=6, y =7;
+    printf("Before x:%d y:%d\n",x,y);
+    swap(&x, &y);
+    printf("After x:%d y:%d",x,y);
+    return 0;
+}
